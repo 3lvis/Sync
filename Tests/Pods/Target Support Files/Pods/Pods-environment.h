8146a80db4eede8fa1f076872e3ecdd41957dbd8
Elvis Nuñez <elvisnunez@me.com>
2015-03-03 13:14:06 +0100

    Update pods

--- Tests/Pods/Target Support Files/Pods/Pods-environment.h
+++ Tests/Pods/Target Support Files/Pods/Pods-environment.h
@@ -39,8 +39,8 @@
 // NSManagedObject-HYPPropertyMapper
 #define COCOAPODS_POD_AVAILABLE_NSManagedObject_HYPPropertyMapper
 #define COCOAPODS_VERSION_MAJOR_NSManagedObject_HYPPropertyMapper 2
-#define COCOAPODS_VERSION_MINOR_NSManagedObject_HYPPropertyMapper 8
-#define COCOAPODS_VERSION_PATCH_NSManagedObject_HYPPropertyMapper 1
+#define COCOAPODS_VERSION_MINOR_NSManagedObject_HYPPropertyMapper 9
+#define COCOAPODS_VERSION_PATCH_NSManagedObject_HYPPropertyMapper 0
 
 // NSString-HYPNetworking
 #define COCOAPODS_POD_AVAILABLE_NSString_HYPNetworking
