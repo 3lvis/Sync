599f95b2dc5e4945df6e3d28cf44249e9054eb98
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-03-20 20:40:36 +0100

    Placed main views

--- Examples/DesignerNews/DesignerNews/ViewController.m
+++ Examples/DesignerNews/DesignerNews/ViewController.m
@@ -1,8 +1,12 @@
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString * const CellIdentifier = @"Cell";
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) UITableView *tableView;
+@property CGFloat deviceWidth;
+@property CGFloat deviceHeight;
 
 @end
 
@@ -11,6 +15,57 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    [self preferredStatusBarStyle];
+
+    self.deviceWidth = [UIScreen mainScreen].bounds.size.width;
+    self.deviceHeight = [UIScreen mainScreen].bounds.size.height;
+
+    [self setAllViewsInPlace];
+
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+    self.tableView.delegate = self;
+
+    [self.view addSubview:self.tableView];
+}
+
+#pragma mark - UITableView delegate methods
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+    return cell;
+}
+
+#pragma mark - Helper methods
+
+- (void)setAllViewsInPlace
+{
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.deviceWidth, 90)];
+    headerView.backgroundColor = [UIColor colorWithRed:0.2 green:0.46 blue:0.84 alpha:1];
+    [self.view addSubview:headerView];
+
+    UILabel *labelTitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, self.deviceWidth, headerView.frame.size.height - 15)];
+    labelTitle.text = @"Designer News";
+    labelTitle.font = [UIFont fontWithName:@"AvenirNextCondensed-Medium" size:34];
+    labelTitle.textColor = [UIColor whiteColor];
+    labelTitle.textAlignment = NSTextAlignmentCenter;
+    [headerView addSubview:labelTitle];
+
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 90, self.deviceWidth, self.deviceHeight - 90) style:UITableViewStylePlain];
+}
+
+#pragma mark - UIStatusBar methods
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
 }
 
 @end
