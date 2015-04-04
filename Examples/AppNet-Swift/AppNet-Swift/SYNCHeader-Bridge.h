8f6ee131a7cad219b91027a9b8940bb39f46e716
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-04 16:42:49 +0200

    Added Bridge and configured code

---
+++ Examples/AppNet-Swift/AppNet-Swift/SYNCHeader-Bridge.h
@@ -0,0 +1,8 @@
+#ifndef AppNet_Swift_SYNCHeader_Bridge_h
+#define AppNet_Swift_SYNCHeader_Bridge_h
+
+#import <Sync/Sync.h>
+#import <DATAStack/DATAStack.h>
+#import <DATASource/DATASource.h>
+
+#endif
--- Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
+++ Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
@@ -1,6 +1,22 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    var dataStack: DATAStack!
+
+    // MARK: Initializers
+
+    required init(dataStack: DATAStack)
+    {
+        super.init(nibName: nil, bundle: nil);
+        self.dataStack = dataStack
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(nibName: nil, bundle: nil);
+    }
+
+    // MARK: Viewcycle
 
     override func viewDidLoad()
     {
