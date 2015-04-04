16130617a7e7abfb696c2f71e04d72420d65967b
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-04 20:07:11 +0200

    Updated, no tests, no storyboard

--- Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
+++ Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
@@ -26,11 +26,10 @@ class ViewController: UITableViewController, UITableViewDelegate, UITableViewDat
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: UITableViewCell! = self.tableView.dequeueReusableCellWithIdentifier(SYNCCellIdentifier) as UITableViewCell
-        println(self.arrayWithData[indexPath.row])
         let data: Data = self.arrayWithData[indexPath.row] as Data
 
         cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: SYNCCellIdentifier)
-
+        
         cell.textLabel!.text = data.text
         cell.textLabel!.numberOfLines = 1000
         cell.detailTextLabel!.text = data.user.username
