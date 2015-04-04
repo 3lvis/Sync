0370f49545bf29417f12536c1e48a5008c31d901
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-04 20:14:43 +0200

    Reordered, no Landscape

--- Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
+++ Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
@@ -18,6 +18,29 @@ class ViewController: UITableViewController, UITableViewDelegate, UITableViewDat
         super.init(nibName: nil, bundle: nil);
     }
 
+    // MARK: View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.arrayWithData = Array()
+
+        self.title = "AppNet"
+
+        self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: SYNCCellIdentifier)
+        self.tableView.allowsSelection = false
+
+        fetchCurrentObjects()
+
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "finishedFetchingPosts", name: SYNCReloadTableNotification, object: nil)
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        fetchNewData()
+    }
+
     // MARK: TableView methods
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,7 +52,7 @@ class ViewController: UITableViewController, UITableViewDelegate, UITableViewDat
         let data: Data = self.arrayWithData[indexPath.row] as Data
 
         cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: SYNCCellIdentifier)
-        
+
         cell.textLabel!.text = data.text
         cell.textLabel!.numberOfLines = 1000
         cell.detailTextLabel!.text = data.user.username
@@ -56,31 +79,9 @@ class ViewController: UITableViewController, UITableViewDelegate, UITableViewDat
         let request = NSFetchRequest(entityName: "Data")
         request.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
         self.arrayWithData = self.dataStack.mainContext.executeFetchRequest(request, error: nil) as Array
-
+        
         self.tableView.reloadData()
     }
 
-    // MARK: View Lifecycle
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.arrayWithData = Array()
-
-        self.title = "AppNet"
-
-        self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: SYNCCellIdentifier)
-        self.tableView.allowsSelection = false
-
-        fetchCurrentObjects()
-
-        let notificationCenter = NSNotificationCenter.defaultCenter()
-        notificationCenter.addObserver(self, selector: "finishedFetchingPosts", name: SYNCReloadTableNotification, object: nil)
-    }
-
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(true)
-        fetchNewData()
-    }
 }
 
