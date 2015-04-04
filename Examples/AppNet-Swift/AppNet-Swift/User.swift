7611247a89961c1a0632a88d09913dabd1ad997f
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-04 19:23:53 +0200

    Removed attributions

--- Examples/AppNet-Swift/AppNet-Swift/User.swift
+++ Examples/AppNet-Swift/AppNet-Swift/User.swift
@@ -1,11 +1,3 @@
-//
-//  User.swift
-//  AppNet-Swift
-//
-//  Created by Kostiantyn Koval on 04/04/15.
-//  Copyright (c) 2015 Ramon Gilabert. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
