e2aed57c6c37e63d8d1317849a527f5f489492df
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-05 19:05:37 +0200

    Possible blocks solution

--- Examples/AppNet-Swift/AppNet-Swift/Networking.swift
+++ Examples/AppNet-Swift/AppNet-Swift/Networking.swift
@@ -7,29 +7,29 @@ class Networking: NSObject {
   let dataStack: DATAStack
 
   required init(dataStack: DATAStack) {
-      self.dataStack = dataStack
-      super.init()
+    self.dataStack = dataStack
+    super.init()
   }
 
-  func fetchNewContent() {
-      let urlAppNet = NSURL(string: SYNCAppNetURL)
-      let request = NSURLRequest(URL: urlAppNet!)
-      let operationQueue = NSOperationQueue()
-      NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) { (_, data, error) in
-          if error != nil {
-              let alertController = UIAlertController(title: "Ooops!", message: "There was a connection error. \(error)", preferredStyle: .Alert)
-              let alertAction = UIAlertAction(title: "OK", style: .Default, handler: { action in
-                  alertController.dismissViewControllerAnimated(true, completion: nil)
-              })
+  func fetchNewContent(completion: () -> Void) {
+    let urlAppNet = NSURL(string: SYNCAppNetURL)
+    let request = NSURLRequest(URL: urlAppNet!)
+    let operationQueue = NSOperationQueue()
+    NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) { (_, data, error) in
+      if error != nil {
+        let alertController = UIAlertController(title: "Ooops!", message: "There was a connection error. \(error)", preferredStyle: .Alert)
+        let alertAction = UIAlertAction(title: "OK", style: .Default, handler: { action in
+          alertController.dismissViewControllerAnimated(true, completion: nil)
+        })
 
-              alertController.addAction(alertAction)
-          } else {
-              let serializationJSON: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
+        alertController.addAction(alertAction)
+      } else {
+        let serializationJSON: AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil)
 
-              Sync.changes(serializationJSON.valueForKey("data") as NSArray, inEntityNamed: "Data", dataStack: self.dataStack, completion: { [unowned self] error in
-                  
-              })
-          }
+        Sync.changes(serializationJSON.valueForKey("data") as NSArray, inEntityNamed: "Data", dataStack: self.dataStack, completion: { [unowned self] error in
+          completion()
+        })
       }
+    }
   }
 }
--- Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
+++ Examples/AppNet-Swift/AppNet-Swift/ViewController.swift
@@ -11,76 +11,74 @@ class ViewController: UITableViewController, UITableViewDelegate, UITableViewDat
   // MARK: Initializers
 
   required init(dataStack: DATAStack) {
-      super.init(nibName: nil, bundle: nil);
-      self.dataStack = dataStack
+    super.init(nibName: nil, bundle: nil);
+    self.dataStack = dataStack
   }
 
   required init(coder aDecoder: NSCoder) {
-      super.init(nibName: nil, bundle: nil);
+    super.init(nibName: nil, bundle: nil);
   }
 
   // MARK: View Lifecycle
 
   override func viewDidLoad() {
-      super.viewDidLoad()
+    super.viewDidLoad()
 
-      self.arrayWithData = Array()
+    self.arrayWithData = Array()
 
-      self.title = "AppNet"
+    self.title = "AppNet"
 
-      self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: SYNCCellIdentifier)
-      self.tableView.allowsSelection = false
+    self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: SYNCCellIdentifier)
+    self.tableView.allowsSelection = false
 
-      fetchCurrentObjects()
+    fetchCurrentObjects()
 
-      //let notificationCenter = NSNotificationCenter.defaultCenter()
-      //notificationCenter.addObserver(self, selector: "finishedFetchingPosts", name: SYNCReloadTableNotification, object: nil)
+    //let notificationCenter = NSNotificationCenter.defaultCenter()
+    //notificationCenter.addObserver(self, selector: "finishedFetchingPosts", name: SYNCReloadTableNotification, object: nil)
   }
 
   override func viewDidAppear(animated: Bool) {
-      super.viewDidAppear(true)
-      fetchNewData()
+    super.viewDidAppear(true)
+    fetchNewData()
   }
 
   // MARK: TableView methods
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return arrayWithData.count
+    return arrayWithData.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-      var cell = self.tableView.dequeueReusableCellWithIdentifier(SYNCCellIdentifier) as UITableViewCell
-      let data = self.arrayWithData[indexPath.row]
+    var cell = self.tableView.dequeueReusableCellWithIdentifier(SYNCCellIdentifier) as UITableViewCell
+    let data = self.arrayWithData[indexPath.row]
 
-      cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: SYNCCellIdentifier)
+    cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: SYNCCellIdentifier)
 
-      cell.textLabel?.text = data.text
-      cell.textLabel?.numberOfLines = 1000
-      cell.detailTextLabel?.text = data.user.username
+    cell.textLabel?.text = data.text
+    cell.textLabel?.numberOfLines = 1000
+    cell.detailTextLabel?.text = data.user.username
 
-      return cell
+    return cell
   }
 
   // MARK: Notification
 
-  func finishedFetchingPosts() {
-      fetchCurrentObjects()
-  }
-
   // MARK: Networking methods
 
   func fetchNewData() {
-      self.networking = Networking(dataStack: self.dataStack)
-      self.networking.fetchNewContent()
+    self.networking = Networking(dataStack: self.dataStack)
+    self.networking.fetchNewContent { () -> Void in
+      self.fetchCurrentObjects()
+    }
   }
 
   // MARK: Model methods
 
   func fetchCurrentObjects() {
-      let request = NSFetchRequest(entityName: "Data")
-      request.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
-      self.arrayWithData = self.dataStack.mainContext.executeFetchRequest(request, error: nil) as Array
+    let request = NSFetchRequest(entityName: "Data")
+    request.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
+    self.arrayWithData = self.dataStack.mainContext.executeFetchRequest(request, error: nil) as Array
       
-      tableView.reloadData()
+    tableView.reloadData()
   }
 }
