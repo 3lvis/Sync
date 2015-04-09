3a02e340fb2c8afdd1ce7496a4de3e9dfe2991ce
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-09 20:26:33 +0200

    Use two spaces

--- Source/Sync.swift
+++ Source/Sync.swift
@@ -6,30 +6,27 @@ let CustomRemoteKey = "hyper.remoteKey"
 
 private extension NSEntityDescription {
 
-    func sync_localKey() {
-        var localKey: String?
+  func sync_localKey() {
+    var localKey: String?
 
-        for (key, attributedDescription) in self.propertiesByName {
-            if let userInfo = attributedDescription.userInfo {
-                if let customPrimaryKey = userInfo[SyncCustomPrimaryKey] as? String {
-                    if customPrimaryKey == "YES" {
-                        localKey = key as? String
-                    }
-                }
-            }
+    for (key, attributedDescription) in self.propertiesByName {
+      if let userInfo = attributedDescription.userInfo {
+        if let customPrimaryKey = userInfo[SyncCustomPrimaryKey] as? String {
+          if customPrimaryKey == "YES" {
+              localKey = key as? String
+          }
         }
-
+      }
     }
-
+  }
 }
 
 public extension NSManagedObject {
 
-    private func sync_copyInContext(context: NSManagedObjectContext) -> NSManagedObject? {
-        let entity = NSEntityDescription.entityForName(self.entity.name!,
-            inManagedObjectContext: context)
+  private func sync_copyInContext(context: NSManagedObjectContext) -> NSManagedObject? {
+    let entity = NSEntityDescription.entityForName(self.entity.name!,
+        inManagedObjectContext: context)
 
-            return nil
-    }
-
+    return nil
+  }
 }
