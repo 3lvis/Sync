400beedde5b2884d2afde2da994747300fd2da6c
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-04-12 21:21:29 +0200

    Delete some spaces

--- Source/Sync.swift
+++ Source/Sync.swift
@@ -154,17 +154,13 @@ extension NSManagedObject {
       let entity = NSEntityDescription.entityForName(entityName!, inManagedObjectContext: self.managedObjectContext!)
 
       if let filteredObjectDictionary = dictionary[relationshipName] as? [NSObject : AnyObject] {
-
         if let remoteKey: String = entity?.sync_remoteKey() {
-
           let remoteID: AnyObject? = filteredObjectDictionary[remoteKey]
-
             if let updatedObject = Sync.safeObjectInContext(self.managedObjectContext!,
               entityName: entityName!,
               remoteID: remoteID!) {
                 updatedObject.hyp_fillWithDictionary(filteredObjectDictionary)
                 self.setValue(updatedObject, forKey: relationship.name)
-
             } else if let newObject = NSEntityDescription.insertNewObjectForEntityForName(entityName!,
               inManagedObjectContext: self.managedObjectContext!) as? NSManagedObject {
                 newObject.hyp_fillWithDictionary(filteredObjectDictionary)
