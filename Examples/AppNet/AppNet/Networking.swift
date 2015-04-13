fd7dd9c31378e296c8bacaf02519639eadcc1af2
Christoffer Winterkvist <chris@zenangst.com>
2015-04-13 09:43:14 +0200

    Add if conditional

--- Examples/AppNet/AppNet/Networking.swift
+++ Examples/AppNet/AppNet/Networking.swift
@@ -10,22 +10,23 @@ class Networking {
 
   func fetchItems(completion: (NSError?) -> Void) {
 
-    let urlAppNet = NSURL(string: AppNetURL)
-    let request = NSURLRequest(URL: urlAppNet!)
-    let operationQueue = NSOperationQueue()
+    if let urlAppNet = NSURL(string: AppNetURL) {
+      let request = NSURLRequest(URL: urlAppNet)
+      let operationQueue = NSOperationQueue()
 
-    NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) { [unowned self] _, data, error in
-      if let data = data, json = NSJSONSerialization.JSONObjectWithData(data,
-        options: NSJSONReadingOptions.MutableContainers,
-        error: nil) as? Dictionary<String, AnyObject> {
-          Sync.changes(json["data"] as! Array,
-            inEntityNamed: "Data",
-            dataStack: self.dataStack,
-            completion: { error in
-              completion(error)
-          })
-      } else {
-        completion(error)
+      NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) { [unowned self] _, data, error in
+        if let data = data, json = NSJSONSerialization.JSONObjectWithData(data,
+          options: NSJSONReadingOptions.MutableContainers,
+          error: nil) as? Dictionary<String, AnyObject> {
+            Sync.changes(json["data"] as! Array,
+              inEntityNamed: "Data",
+              dataStack: self.dataStack,
+              completion: { error in
+                completion(error)
+            })
+        } else {
+          completion(error)
+        }
       }
     }
   }
