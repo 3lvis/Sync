95af1fe06612c67c44e72af417f2f2a9969c4c9f
Elvis Nuñez <elvisnunez@me.com>
2015-09-24 21:00:40 +0200

    Xcode 7 + Swift 2

--- Examples/AppNet/AppNet/Networking.swift
+++ Examples/AppNet/AppNet/Networking.swift
@@ -15,9 +15,8 @@ class Networking {
       let operationQueue = NSOperationQueue()
 
       NSURLConnection.sendAsynchronousRequest(request, queue: operationQueue) { [unowned self] _, data, error in
-        if let data = data, json = NSJSONSerialization.JSONObjectWithData(data,
-          options: NSJSONReadingOptions.MutableContainers,
-          error: nil) as? Dictionary<String, AnyObject> {
+        if let data = data, json = (try? NSJSONSerialization.JSONObjectWithData(data,
+          options: NSJSONReadingOptions.MutableContainers)) as? Dictionary<String, AnyObject> {
             Sync.changes(json["data"] as! Array,
               inEntityNamed: "Data",
               dataStack: self.dataStack,
--- Examples/AppNet/AppNet/ViewController.swift
+++ Examples/AppNet/AppNet/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UITableViewController {
     super.init(nibName: nil, bundle: nil);
   }
 
-  required init!(coder aDecoder: NSCoder!) {
+  required init!(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
 
@@ -51,31 +51,25 @@ class ViewController: UITableViewController {
   func fetchCurrentObjects() {
     let request = NSFetchRequest(entityName: "Data")
     request.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
-    items = dataStack.mainContext.executeFetchRequest(request, error: nil) as! [Data]
+    items = (try! dataStack.mainContext.executeFetchRequest(request)) as! [Data]
 
     tableView.reloadData()
   }
-}
-
-extension ViewController: UITableViewDataSource {
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return items.count
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    var cell = self.tableView.dequeueReusableCellWithIdentifier(CellIdentifier) as! UITableViewCell
+    let cell = self.tableView.dequeueReusableCellWithIdentifier(CellIdentifier)
     let data = self.items[indexPath.row]
-
-    cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: CellIdentifier)
-
-    cell.textLabel?.text = data.text
+    cell?.textLabel?.text = data.text
 
     // Workaround: The proper value of `numberOfLines` should be 0
     // but there's a weird bug that causes UITableView to go crazy
-    cell.textLabel?.numberOfLines = 1
-    cell.detailTextLabel?.text = data.user.username
+    cell?.textLabel?.numberOfLines = 1
+    cell?.detailTextLabel?.text = data.user.username
 
-    return cell
+    return cell!
   }
 }
