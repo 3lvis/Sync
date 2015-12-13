b442ead12b9d3678209b36acc0f0eae61781d2e1
Elvis Nuñez <elvisnunez@me.com>
2015-12-13 16:42:21 +0100

    Simplify AppDelegate

--- AppNet/AppDelegate.swift
+++ AppNet/AppDelegate.swift
@@ -4,22 +4,13 @@ import DATAStack
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     lazy var dataStack: DATAStack = DATAStack()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        application.setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
-
-        let appearance = UINavigationBar.appearance()
-        appearance.barTintColor = UIColor(red:0.83, green:0.43, blue:0.36, alpha:1)
-        appearance.titleTextAttributes = [NSFontAttributeName : UIFont(name: "AvenirNext-DemiBold", size: 20)!,
-            NSForegroundColorAttributeName : UIColor.whiteColor()]
-
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.rootViewController = UINavigationController(rootViewController: ViewController(dataStack: dataStack))
         window?.makeKeyAndVisible()
-
         return true
     }
 
