5a22c96eeafe3affb1a081e8459d2e276986c58a
Elvis Nuñez <elvisnunez@me.com>
2016-01-20 17:28:25 +0100

    Add missing methods

--- Source/Sync.swift
+++ Source/Sync.swift
@@ -5,37 +5,19 @@ import NSManagedObject_HYPPropertyMapper
 import DATAStack
 
 @objc public class Sync: NSObject {
-    public class func changes(changes: [AnyObject], inEntityNamed entityName: String, parent: NSManagedObject, dataStack: DATAStack, completion: Void -> Void) {
-        dataStack.performInNewBackgroundContext { backgroundContext in
-//            let safeParent = parent.sync_copyInContext(backgroundContext)
-//            let entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: backgroundContext)!
-//            let relationships = entity.relationshipsWithDestinationEntity(parent.entity)
-//            let predicate = NSPredicate(format: "%K = %@", relationships.first!.name, safeParent)
-        }
+    public class func changes(changes: NSArray, inEntityNamed entityName: String, dataStack: DATAStack, completion: ((error: NSError?) -> Void)?) {
+
     }
 
-    public class func changes(changes: [AnyObject], inEntityNamed entityName: String, predicate: NSPredicate?, parent: NSManagedObject?, inContext context: NSManagedObjectContext, dataStack: DATAStack, completion: (Void -> Void)?) {
-        let entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: context)!
-        let localKey = entity.sync_localKey()
-        let remoteKey = entity.sync_remoteKey()
-        let shouldLookForParent = (parent == nil && predicate == nil)
-        if shouldLookForParent {
-            // let parentEntity = entity.sync_parentEntity()
-            // predicate = NSPredicate(format: "%K = nil", parentEntity.name)
-        }
+    public class func changes(changes: NSArray, inEntityNamed entityName: String, predicate: NSPredicate, dataStack: DATAStack, completion: ((error: NSError?) -> Void)?) {
 
-        DATAFilter.changes(changes, inEntityNamed: entityName, predicate: predicate, operations: .All, localKey: localKey, remoteKey: remoteKey, context: context, inserted: { JSON in
-            let created = NSEntityDescription.insertNewObjectForEntityForName(entityName, inManagedObjectContext: context)
-            created.hyp_fillWithDictionary(JSON)
-            // created.sync_processRelationshipsUsingDictionary(JSON, andParent: parent, dataStack: dataStack)
-        }) { (JSON, updatedObject) in
-            updatedObject.hyp_fillWithDictionary(JSON)
-            // updatedObject.sync_processRelationshipsUsingDictionary(JSON, andParent: parent, dataStack: dataStack)
-        }
+    }
 
-        try! context.save()
-        dataStack.persistWithCompletion {
-            completion?()
-        }
+    public class func changes(changes: NSArray, inEntityNamed entityName: String, parent: NSManagedObject, dataStack: DATAStack, completion: ((error: NSError?) -> Void)?) {
+
+    }
+
+    public class func changes(changes: NSArray, inEntityNamed entityName: String, predicate: NSPredicate, parent: NSManagedObject, inContext context:NSManagedObjectContext, dataStack: DATAStack, completion: ((error: NSError?) -> Void)?) {
+
     }
 }
