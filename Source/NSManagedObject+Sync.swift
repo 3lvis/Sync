a239dbf628d00a3bd0039e81d725f48e7a9c11ef
Elvis Nuñez <elvisnunez@me.com>
2016-01-21 12:56:50 +0100

    Even more type safety

--- Source/NSManagedObject+Sync.swift
+++ Source/NSManagedObject+Sync.swift
@@ -47,23 +47,21 @@ public extension NSManagedObject {
         let inverseEntityName = relationship.inverseRelationship?.name
         let inverseIsToMany = relationship.inverseRelationship?.toMany ?? false
         let hasValidManyToManyRelationship = parent != nil && parentEntityName != nil && inverseIsToMany && parentEntityName! == childEntityName
-        if let children = objectDictionary.andy_valueForKey(relationshipName) as? NSArray {
+        if let children = objectDictionary.andy_valueForKey(relationshipName) as? [[String : AnyObject]] {
             var childPredicate: NSPredicate? = nil
             if inverseIsToMany {
                 let entity = NSEntityDescription.entityForName(childEntityName, inManagedObjectContext: self.managedObjectContext!)!
                 let destinationRemoteKey = entity.sync_remoteKey()
-                let childIDs = children.valueForKey(destinationRemoteKey)
+                let childIDs = (children as NSArray).valueForKey(destinationRemoteKey)
                 let destinationLocalKey = entity.sync_localKey()
                 if childIDs.count > 0 {
-                    childPredicate = NSPredicate(format: "ANY %K IN %@", destinationLocalKey, children.valueForKey(destinationRemoteKey) as! NSObject)
+                    childPredicate = NSPredicate(format: "ANY %K IN %@", destinationLocalKey, childIDs as! NSObject)
                 }
             } else if let inverseEntityName = inverseEntityName {
                 childPredicate = NSPredicate(format: "%K = %@", inverseEntityName, self)
             }
 
-            if let children = children as? [[String : AnyObject]] {
-                Sync.changes(children, inEntityNamed: childEntityName, predicate: childPredicate, parent: self, inContext: self.managedObjectContext!, dataStack: dataStack, completion: nil)
-            }
+            Sync.changes(children, inEntityNamed: childEntityName, predicate: childPredicate, parent: self, inContext: self.managedObjectContext!, dataStack: dataStack, completion: nil)
         } else if hasValidManyToManyRelationship, let parent = parent {
             let relatedObjects = self.mutableSetValueForKey(relationship.name)
             if !relatedObjects.containsObject(parent) {
