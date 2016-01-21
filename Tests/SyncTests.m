0e47abd7546b1ce392f3d26c62937e43a64cb476
Elvis Nuñez <elvisnunez@me.com>
2016-01-21 11:32:13 +0100

    Autoformat

--- Tests/SyncTests.m
+++ Tests/SyncTests.m
@@ -294,8 +294,8 @@
        completion:nil];
 
     NSArray *updatedArray = [self fetchEntity:@"SuperUser"
-                       sortDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"remoteID" ascending:YES]]
-                             inContext:dataStack.mainContext];
+                              sortDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"remoteID" ascending:YES]]
+                                    inContext:dataStack.mainContext];
     XCTAssertEqual(updatedArray.count, 3);
 
     NSManagedObject *updatedUser1 = updatedArray[0];
@@ -308,7 +308,8 @@
     XCTAssertEqualObjects([updatedUser3 valueForKey:@"name"], new[@"name"]);
 
     [dataStack drop];
-}*/
+}
+*/
 
 #pragma mark Recursive
 
@@ -725,14 +726,14 @@
 
 /*
  When having JSONs like this:
-{
-    "id":12345,
-    "name":"My Project",
-    "category_id":12345
-}
- 
+ {
+ "id":12345,
+ "name":"My Project",
+ "category_id":12345
+ }
+
  It will should map category_id with the necesary category object using the ID 12345
-*/
+ */
 
 - (void)testIDRelationshipMapping {
     NSArray *usersDictionary = [self objectsFromJSON:@"users_a.json"];
