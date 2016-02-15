d227a6298f763f5d46a672424254262d768ab9a1
Wain <wain@niceagency.co.uk>
2016-02-15 10:39:38 +0000

    Issue 179. Updated relationship syncing to use the current relationship instead of always taking the 'first' relationship of the entity description relationships. Added associated test.

--- Source/NSManagedObject+Sync.swift
+++ Source/NSManagedObject+Sync.swift
@@ -5,7 +5,7 @@ import NSString_HYPNetworking
 
 public extension NSManagedObject {
     /**
-     Using objectID to fetch an NSManagedObject from a NSManagedContext is quite unsafe, 
+     Using objectID to fetch an NSManagedObject from a NSManagedContext is quite unsafe,
      and has unexpected behaviour most of the time, although it has gotten better throught
      the years, it's a simple method with not many moving parts.
      
@@ -17,10 +17,10 @@ public extension NSManagedObject {
         let entity = NSEntityDescription.entityForName(self.entity.name!, inManagedObjectContext: context)!
         let localKey = entity.sync_localKey()
         let remoteID = self.valueForKey(localKey)
-
+        
         return context.sync_safeObject(self.entity.name!, remoteID: remoteID, parent: nil, parentRelationshipName: nil)!
     }
-
+    
     /**
      Syncs the entity using the received dictionary, maps one-to-many, many-to-many and one-to-one relationships.
      It also syncs relationships where only the id is present, for example if your model is: Company -> Employee,
@@ -32,30 +32,26 @@ public extension NSManagedObject {
      */
     func sync_fillWithDictionary(dictionary: [String : AnyObject], parent: NSManagedObject?, dataStack: DATAStack) {
         self.hyp_fillWithDictionary(dictionary)
-
+        
         let relationships = self.entity.sync_relationships()
         for relationship in relationships {
-            let entity = NSEntityDescription.entityForName(relationship.entity.name!, inManagedObjectContext: self.managedObjectContext!)!
-            let relationships = entity.relationshipsWithDestinationEntity(relationship.destinationEntity!)
-            if relationships.count > 0 {
-                let constructedKeyName = relationships.first!.name.hyp_remoteString().stringByAppendingString("_id")
-                let keyName = relationships.first!.userInfo?[SYNCCustomRemoteKey] as? String ?? constructedKeyName
-                if relationship.toMany {
-                    self.sync_toManyRelationship(relationship, dictionary: dictionary, parent: parent, dataStack: dataStack)
-                } else if relationship.destinationEntity?.name == parent?.entity.name {
-                    let currentParent = self.valueForKey(relationship.name)
-                    if currentParent == nil || parent != nil && !currentParent!.isEqual(parent!) {
-                        self.setValue(parent, forKey: relationship.name)
-                    }
-                } else if let remoteID = dictionary[keyName] where remoteID.isKindOfClass(NSString.self) || remoteID.isKindOfClass(NSNumber.self) {
-                    self.sync_relationshipUsingIDInsteadOfDictionary(relationship, remoteID: remoteID, dataStack: dataStack)
-                } else {
-                    self.sync_toOneRelationship(relationship, dictionary: dictionary, dataStack: dataStack)
+            let constructedKeyName = relationship.name.hyp_remoteString().stringByAppendingString("_id")
+            let keyName = relationship.userInfo?[SYNCCustomRemoteKey] as? String ?? constructedKeyName
+            if relationship.toMany {
+                self.sync_toManyRelationship(relationship, dictionary: dictionary, parent: parent, dataStack: dataStack)
+            } else if relationship.destinationEntity?.name == parent?.entity.name {
+                let currentParent = self.valueForKey(relationship.name)
+                if currentParent == nil || parent != nil && !currentParent!.isEqual(parent!) {
+                    self.setValue(parent, forKey: relationship.name)
                 }
+            } else if let remoteID = dictionary[keyName] where remoteID.isKindOfClass(NSString.self) || remoteID.isKindOfClass(NSNumber.self) {
+                self.sync_relationshipUsingIDInsteadOfDictionary(relationship, remoteID: remoteID, dataStack: dataStack)
+            } else {
+                self.sync_toOneRelationship(relationship, dictionary: dictionary, dataStack: dataStack)
             }
         }
     }
-
+    
     /**
      Syncs the entity's to-many relationship, it will also sync the childs of this relationship.
      - parameter relationship: The relationship to be synced.
@@ -84,7 +80,7 @@ public extension NSManagedObject {
             } else if let inverseEntityName = inverseEntityName {
                 childPredicate = NSPredicate(format: "%K = %@", inverseEntityName, self)
             }
-
+            
             Sync.changes(children, inEntityNamed: childEntityName, predicate: childPredicate, parent: self, inContext: self.managedObjectContext!, dataStack: dataStack, completion: nil)
         } else if hasValidManyToManyRelationship, let parent = parent {
             if relationship.ordered {
@@ -102,7 +98,7 @@ public extension NSManagedObject {
             }
         }
     }
-
+    
     /**
      Syncs relationships where only the id is present, for example if your model is: Company -> Employee,
      and your employee has a company_id, it will try to sync using that ID instead of requiring you to provide the
@@ -114,13 +110,13 @@ public extension NSManagedObject {
     func sync_relationshipUsingIDInsteadOfDictionary(relationship: NSRelationshipDescription, remoteID: AnyObject, dataStack: DATAStack) {
         let entityName = relationship.destinationEntity!.name!
         guard let object = self.managedObjectContext!.sync_safeObject(entityName, remoteID: remoteID, parent: self, parentRelationshipName: relationship.name) else { abort() }
-
+        
         let currentRelationship = self.valueForKey(relationship.name)
         if currentRelationship == nil || !currentRelationship!.isEqual(object) {
             self.setValue(object, forKey: relationship.name)
         }
     }
-
+    
     /**
      Syncs the entity's to-one relationship, it will also sync the child of this entity.
      - parameter relationship: The relationship to be synced.
