71ef281a8dbaff152403db58d65f1b4c28a2cfe4
Christoffer Winterkvist <christoffer@hyper.no>
2016-03-17 01:38:07 +0100

    Refactor NSManagedObjectContext.sync_safeObject
    - Remove force unwrapping of objects and when type casting
    - Fix indentation

--- Source/NSManagedObjectContext+Sync.swift
+++ Source/NSManagedObjectContext+Sync.swift
@@ -3,27 +3,26 @@ import NSEntityDescription_SYNCPrimaryKey
 import NSString_HYPNetworking
 
 public extension NSManagedObjectContext {
-    /**
-     Safely fetches a NSManagedObject in the current context. If no remoteID is provided then it will check for the parent entity and use that. Otherwise it will return nil.
-     - parameter entityName: The name of the Core Data entity.
-     - parameter remoteID: The primary key.
-     - parameter parent: The parent of the object.
-     - parameter parentRelationshipName: The name of the relationship with the parent.
-     - returns: A NSManagedObject contained in the provided context.
-     */
-    public func sync_safeObject(entityName: String, remoteID: AnyObject?, parent: NSManagedObject?, parentRelationshipName: String?) -> NSManagedObject? {
-        if let remoteID = remoteID {
-            let entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: self)!
-            let request = NSFetchRequest(entityName: entityName)
-            let localKey = entity.sync_localKey()
-            request.predicate = NSPredicate(format: "%K = %@", localKey, (remoteID as! NSObject))
-            let objects = try! self.executeFetchRequest(request)
-            return objects.first as? NSManagedObject
-        } else if let parentRelationshipName = parentRelationshipName {
-            // More info: https://github.com/hyperoslo/Sync/pull/72
-            return parent?.valueForKey(parentRelationshipName) as? NSManagedObject
-        } else {
-            return nil
-        }
+  /**
+   Safely fetches a NSManagedObject in the current context. If no remoteID is provided then it will check for the parent entity and use that. Otherwise it will return nil.
+   - parameter entityName: The name of the Core Data entity.
+   - parameter remoteID: The primary key.
+   - parameter parent: The parent of the object.
+   - parameter parentRelationshipName: The name of the relationship with the parent.
+   - returns: A NSManagedObject contained in the provided context.
+   */
+  public func sync_safeObject(entityName: String, remoteID: AnyObject?, parent: NSManagedObject?, parentRelationshipName: String?) -> NSManagedObject? {
+    if let remoteID = remoteID as? NSObject, entity = NSEntityDescription.entityForName(entityName, inManagedObjectContext: self) {
+      let request = NSFetchRequest(entityName: entityName)
+      request.predicate = NSPredicate(format: "%K = %@", entity.sync_localKey(), remoteID)
+      let objects = try! executeFetchRequest(request)
+
+      return objects.first as? NSManagedObject
+    } else if let parentRelationshipName = parentRelationshipName {
+      // More info: https://github.com/hyperoslo/Sync/pull/72
+      return parent?.valueForKey(parentRelationshipName) as? NSManagedObject
+    } else {
+      return nil
     }
+  }
 }
