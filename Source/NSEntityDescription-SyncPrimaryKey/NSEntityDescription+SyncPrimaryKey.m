31050be22a0234eb091119d4ba2f4b6323056ecc
Elvis Nuñez <elvisnunez@me.com>
2017-03-16 19:34:47 +0100

    Add files

---
+++ Source/NSEntityDescription-SyncPrimaryKey/NSEntityDescription+SyncPrimaryKey.m
@@ -0,0 +1,52 @@
+#import "NSEntityDescription+SyncPrimaryKey.h"
+
+#import "NSString+SyncInflections.h"
+
+@implementation NSEntityDescription (SyncPrimaryKey)
+
+- (nonnull NSAttributeDescription *)sync_primaryKeyAttribute {
+    __block NSAttributeDescription *primaryKeyAttribute;
+
+    [self.propertiesByName enumerateKeysAndObjectsUsingBlock:^(NSString *key,
+                                                               NSAttributeDescription *attributeDescription,
+                                                               BOOL *stop) {
+        NSString *isPrimaryKey = attributeDescription.userInfo[SyncCustomLocalPrimaryKey];
+        BOOL hasCustomPrimaryKey = (isPrimaryKey &&
+                                    ([isPrimaryKey isEqualToString:SyncCustomLocalPrimaryKeyValue] || [isPrimaryKey isEqualToString:SyncCustomLocalPrimaryKeyAlternativeValue]) );
+        if (hasCustomPrimaryKey) {
+            primaryKeyAttribute = attributeDescription;
+            *stop = YES;
+        }
+
+        if ([key isEqualToString:SyncDefaultLocalPrimaryKey] || [key isEqualToString:SyncDefaultLocalCompatiblePrimaryKey]) {
+            primaryKeyAttribute = attributeDescription;
+        }
+    }];
+
+    return primaryKeyAttribute;
+}
+
+- (nonnull NSString *)sync_localPrimaryKey {
+    NSAttributeDescription *primaryAttribute = [self sync_primaryKeyAttribute];
+    NSString *localKey = primaryAttribute.name;
+
+    return localKey;
+}
+
+- (nonnull NSString *)sync_remotePrimaryKey {
+    NSAttributeDescription *primaryKeyAttribute = [self sync_primaryKeyAttribute];
+    NSString *remoteKey = primaryKeyAttribute.userInfo[SyncCustomRemoteKey];
+
+    if (!remoteKey) {
+        if ([primaryKeyAttribute.name isEqualToString:SyncDefaultLocalPrimaryKey] || [primaryKeyAttribute.name isEqualToString:SyncDefaultLocalCompatiblePrimaryKey]) {
+            remoteKey = SyncDefaultRemotePrimaryKey;
+        } else {
+            remoteKey = [primaryKeyAttribute.name hyp_snakeCase];
+        }
+
+    }
+
+    return remoteKey;
+}
+
+@end
---
+++ Source/NSString-SyncInflections/NSString+SyncInflections.h
@@ -0,0 +1,9 @@
+@import Foundation;
+
+@interface NSString (SyncInflections)
+
+- (nonnull NSString *)hyp_snakeCase;
+
+- (nullable NSString *)hyp_camelCase;
+
+@end
