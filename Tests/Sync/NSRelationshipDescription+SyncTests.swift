19ff8116069ffad62a5a1657c6064611551c1f2b
Elvis Nuñez <elvisnunez@me.com>
2017-03-21 22:07:27 +0100

    Include unit tests for current

--- Tests/Sync/NSRelationshipDescription+SyncTests.swift
+++ Tests/Sync/NSRelationshipDescription+SyncTests.swift
@@ -3,11 +3,28 @@ import CoreData
 @testable import Sync
 
 class NSRelationshipDescription_SyncTests: XCTestCase {
-    func testCustomKey() {
+    func testOldCustomKey() {
         let dataStack = Helper.dataStackWithModelName("HyperRemoteKey")
 
         if let entity = NSEntityDescription.entity(forEntityName: "Entity", in: dataStack.mainContext) {
-            let dayAttribute = entity.sync_attributes().filter { $0.name == "day" }.first
+            let dayAttribute = entity.sync_attributes().filter { $0.name == "old" }.first
+            if let dayAttribute = dayAttribute {
+                XCTAssertEqual(dayAttribute.customKey, "custom")
+            } else {
+                XCTFail()
+            }
+        } else {
+            XCTFail()
+        }
+
+        dataStack.drop()
+    }
+
+    func testCurrentCustomKey() {
+        let dataStack = Helper.dataStackWithModelName("HyperRemoteKey")
+
+        if let entity = NSEntityDescription.entity(forEntityName: "Entity", in: dataStack.mainContext) {
+            let dayAttribute = entity.sync_attributes().filter { $0.name == "current" }.first
             if let dayAttribute = dayAttribute {
                 XCTAssertEqual(dayAttribute.customKey, "custom")
             } else {
