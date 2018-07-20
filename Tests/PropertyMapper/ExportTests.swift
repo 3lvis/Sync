9b78c5e5bcbe71a8a529062eda77b363ac5d13a6
Elvis Nuñez <elvisnunez@me.com>
2018-07-20 16:12:16 +0200

    Break one test to try

--- Tests/PropertyMapper/ExportTests.swift
+++ Tests/PropertyMapper/ExportTests.swift
@@ -149,7 +149,7 @@ class ExportTests: XCTestCase {
 
 
         let result = user.export(using: .camelCase)
-        XCTAssertEqual(compared as NSDictionary, result as NSDictionary)
+        XCTAssertNotEqual(compared as NSDictionary, result as NSDictionary)
 
         dataStack.drop()
     }
