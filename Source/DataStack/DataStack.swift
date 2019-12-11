61cf95a94a54b5cefbac455f9be99c4373df3672
Rob Lester <rlester@users.noreply.github.com>
2019-12-11 14:21:15 -0700

    Checking that the context that is being rest for a disposable context is indeed a disposable context

--- Source/DataStack/DataStack.swift
+++ Source/DataStack/DataStack.swift
@@ -29,6 +29,8 @@ import CoreData
 
     private let backgroundContextName = "DataStack.backgroundContextName"
 
+    private let disposableContextName = "DataStack.disposableContextName"
+
     /**
      The context for the main queue. Please do not use this to mutate data, use `performInNewBackgroundContext`
      instead.
@@ -207,6 +209,7 @@ import CoreData
      */
     @objc public func newDisposableMainContext() -> NSManagedObjectContext {
         let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.name = disposableContextName
         context.persistentStoreCoordinator = self.disposablePersistentStoreCoordinator
         context.undoManager = nil
 
@@ -369,9 +372,12 @@ import CoreData
 
     // Can't be private, has to be internal in order to be used as a selector.
     @objc func newDisposableMainContextWillSave(_ notification: Notification) {
-        if let context = notification.object as? NSManagedObjectContext {
-            context.reset()
+        let context = notification.object as? NSManagedObjectContext
+        guard context?.name == disposableContextName else {
+            return
         }
+
+        context?.reset()
     }
 
     // Can't be private, has to be internal in order to be used as a selector.
@@ -380,7 +386,7 @@ import CoreData
         guard context?.name == backgroundContextName else {
             return
         }
-        
+
         if Thread.isMainThread && TestCheck.isTesting == false {
             throw NSError(info: "Background context saved in the main thread. Use context's `performBlock`", previousError: nil)
         } else {
@@ -430,7 +436,7 @@ extension NSPersistentStoreCoordinator {
                 }
             }
 
-            let options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true, NSSQLitePragmasOption: ["journal_mode": "DELETE"]] as [AnyHashable : Any]
+            let options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true, NSSQLitePragmasOption: ["journal_mode": "DELETE"]] as [AnyHashable: Any]
             do {
                 try self.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: options)
             } catch {
@@ -495,13 +501,13 @@ extension FileManager {
     /// The directory URL for the sqlite file.
     public static var sqliteDirectoryURL: URL {
         #if os(tvOS)
-        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).last!
-        #else
-        if TestCheck.isTesting {
             return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).last!
-        } else {
-            return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
-        }
+        #else
+            if TestCheck.isTesting {
+                return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).last!
+            } else {
+                return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+            }
         #endif
     }
 }
